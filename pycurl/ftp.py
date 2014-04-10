6af1060776ac05fdec8ea2c748772db9637a7bbb
blacktea <blacktea@ubuntu.(none)>
2014-04-10 08:57:37 -0700

    add

--- pycurl/ftp.py
+++ pycurl/ftp.py
@@ -20,26 +20,30 @@ def ftp_upload( file_name ):
     ftp = FTP()
     bufsize = 1024*5
     ftp.set_debuglevel(2)
-    ftp.connect('185.28.20.238','21')
-    ftp.login('u737107852','114763xj')
-    ftp.cwd('photo')
+    try:
+        ftp.connect('185.28.20.238','21')
+        ftp.login('u737107852','114763xj')
+        ftp.cwd('photo')
+    except Exception ,e:
+        print '链接失败'
+        return False
+
     file_handler = open(file_name,'rb')
     try:
-        ftp.storbinary('STOR %s'
-                %os.path.basename(i).split('.')[0].replace('=','')+'.jpg',file_handler,bufsize)
+        ftp.storbinary('STOR %s' %os.path.basename(file_name).split('.')[0].replace('=','')+'.jpg',file_handler,bufsize)
         file_handler.close()
     except Exception ,e:
-        print '传输失败'
+        print '传输失败',e
         ftp.close()
         return False
     os.system('rm '+file_name)
     return True
 def main(arg):
     while True:
-        for a,b,c in os.walk('./照片'):
+        for a,b,c in os.walk('./'):
             for i in c:
                 if i is not 'ftp.py' :
-                    print i
+                    ftp_upload(i)
         time.sleep(600)
             
 if __name__ == '__main__':
